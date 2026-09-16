c05cf8023fdb8098abe4966e8be76b54f1d97eba
tuanpham21105 <tuan.phamminh9936@gmail.com>
2026-09-16 17:17:53 +0700

    first commit

---
+++ src/main/java/com/demo/retrieve_weather_data_app/RetrieveWeatherDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.retrieve_weather_data_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrieveWeatherDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrieveWeatherDataAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/retrieve_weather_data_app/RetrieveWeatherDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.retrieve_weather_data_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrieveWeatherDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
